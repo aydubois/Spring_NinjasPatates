b9f79bb962de6830fdab35ffc5aace295a5aa6bd
NeoNagrarok <etienne.remi@gmail.com>
2021-07-19 16:07:31 +0200

    Fix User import

--- user/src/main/java/org/abr/audreybr/dao/ItemRepository.java
+++ user/src/main/java/org/abr/audreybr/dao/ItemRepository.java
@@ -2,7 +2,6 @@ package org.abr.audreybr.dao;
 
 import org.abr.audreybr.entity.Chouille;
 import org.abr.audreybr.entity.Item;
-import org.abr.audreybr.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
