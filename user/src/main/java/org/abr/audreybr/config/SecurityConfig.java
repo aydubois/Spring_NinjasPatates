f5fea840ac05266faedf1c0cbd5ea0ec6282fb79
Nicolas <nicolas@hotmail.fr>
2021-07-22 11:16:41 +0200

    connexion security with bdd ok

--- user/src/main/java/org/abr/audreybr/config/SecurityConfig.java
+++ user/src/main/java/org/abr/audreybr/config/SecurityConfig.java
@@ -1,15 +1,19 @@
-package org.abr.audreybr.config;
 
 import org.abr.audreybr.security.AppAuthProvider;
 import org.abr.audreybr.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
@@ -18,19 +22,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserService userDetailsService;
+
+  //  @Autowired
+  //  private AccessDeniedHandler accessDeniedHandler;
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
 
-    UserService userDetailsService;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-    //  @Autowired
-    //  private AccessDeniedHandler accessDeniedHandler;
-
+        http.csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new Http403ForbiddenEntryPoint() {
+                })
+                .and()
+                .authenticationProvider(getProvider())
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .successHandler(new AuthentificationLoginSuccessHandler())
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(new AuthentificationLogoutSuccessHandler())
+                .invalidateHttpSession(true)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/logout").permitAll()
+                .antMatchers("/user").authenticated()
+                .anyRequest().permitAll();
+    }
 
     private class AuthentificationLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
--- user/src/main/java/org/abr/audreybr/dao/UserRepository.java
+++ user/src/main/java/org/abr/audreybr/dao/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query(" select u from User u " +
