74ab59d5399bdf4a3764713c3390a0e9b7f899a8
NeoNagrarok <etienne.remi@gmail.com>
2021-07-22 14:30:30 +0200

    Added alter and update for Person table for security purpose

---
+++ sql/updateValuesAfterAlterSecurity.sql
@@ -0,0 +1,72 @@
+UPDATE  Person 
+SET 
+    `username` = 'Remi',
+    `role`= 'admin',
+    `enabled` = 1
+WHERE `Id_Person` = 1;
+UPDATE  Person 
+SET 
+    `username` = 'Ponyo',
+    `role`= 'admin',
+    `enabled` = 1
+WHERE `Id_Person` = 2;
+UPDATE  Person 
+SET 
+    `username` = 'Ali',
+    `role`= 'admin',
+    `enabled` = 1
+WHERE `Id_Person` = 3;
+UPDATE  Person 
+SET 
+    `username` = 'Arikoh',
+    `role`= 'admin',
+    `enabled` = 1
+WHERE `Id_Person` = 4;
+UPDATE  Person 
+SET 
+    `username` = 'Shlygly',
+    `role`= 'user',
+    `enabled` = 1
+WHERE `Id_Person` = 5;
+UPDATE  Person 
+SET 
+    `username` = 'Tommy',
+    `role`= 'admin',
+    `enabled` = 1
+WHERE `Id_Person` = 6;
+UPDATE  Person 
+SET 
+    `username` = 'Punkadelistic',
+    `role`= 'user',
+    `enabled` = 1
+WHERE `Id_Person` = 7;
+UPDATE  Person 
+SET 
+    `username` = 'Clém',
+    `role`= 'user',
+    `enabled` = 0
+WHERE `Id_Person` = 8;
+UPDATE  Person 
+SET 
+    `username` = 'PiL',
+    `role`= 'user',
+    `enabled` = 1
+WHERE `Id_Person` = 9;
+UPDATE  Person 
+SET 
+    `username` = 'Noman',
+    `role`= 'user',
+    `enabled` = 1
+WHERE `Id_Person` = 10;
+UPDATE  Person 
+SET 
+    `username` = 'Blah',
+    `role`= 'user',
+    `enabled` = 0
+WHERE `Id_Person` = 11;
+UPDATE  Person 
+SET 
+    `username` = 'Plop',
+    `role`= 'user',
+    `enabled` = 0
+WHERE `Id_Person` = 12;
